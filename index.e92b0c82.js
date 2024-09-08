d26d65381268b41a1740a39ebb1af1882227ee59
alex_motalex <tokyopac@gmail.com>
2024-09-08 22:51:19 +0300

    make build

---
+++ index.e92b0c82.js
@@ -0,0 +1,2 @@
+var e=document.body.firstElementChild,o=e.tHead,l=e.tFoot,t=e.tBodies[0];function r(e){Array.from(e.rows).forEach(function(e){var o=e.cells[1].cloneNode(!0);e.cells[e.cells.length-1].before(o)})}r(o),r(l),r(t);
+//# sourceMappingURL=index.e92b0c82.js.map
